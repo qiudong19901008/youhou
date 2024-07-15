621136f8e4bac9ceb428ed4ba682b1284f4fd787
qiud <qiutong19901008@gmail.com>
2024-07-15 17:33:08 +0800

    '1'

--- src/xiaohongshu/DataExporter.ts
+++ src/xiaohongshu/DataExporter.ts
@@ -2,12 +2,13 @@ import Base from "./Base";
 
 
 type RowType = [
-    // '笔记标题', '笔记链接', '作者', '作者链接', '点赞数'
+    // '笔记标题', '笔记链接', '作者', '作者链接', '点赞数', '是否违规'
     title:string,
     url:string,
     authorName:string,
     authorUrl:string,
     likeCount:string,
+    illegal:string,
 ]
 
 interface DataExporterConfigType{
@@ -50,7 +51,7 @@ export default class DataExporter extends Base{
        this._downloadBtn = this._creatDownloadBtn(this._getFilename());
        
         // 添加表头
-       this._data.push(['笔记标题', '笔记链接', '作者', '作者链接', '点赞数']);
+       this._data.push(['笔记标题', '笔记链接', '作者', '作者链接', '点赞数', '是否违规']);
        // 第一次加载，提取笔记内容数据
        this._extractNoteData(noteElementFlagAttr);
        // 监听页面滚动事件，当加载更多内容时，提取更多数据
@@ -137,17 +138,19 @@ export default class DataExporter extends Base{
             return;
         }
         // 提取标题和笔记链接
-        const titleElement = ele.querySelector('a.title span') as HTMLSpanElement;
+        // const titleElement = ele.querySelector('a.title span') as HTMLSpanElement;
+        const titleElement = ele.querySelector('.footer a.title span') as HTMLSpanElement;
         let title = titleElement ? titleElement.innerText : '';
         // 把#号替换掉，防止csv导出错误
         title = title.replace(/#/g,'jin');
         title = title.replace(/,/g, 'douhao')
-        const noteLink = titleElement ? 'https://www.xiaohongshu.com' + titleElement.closest('a.title')?.getAttribute('href') : '';
+        // const noteLink = titleElement ? 'https://www.xiaohongshu.com' + titleElement.closest('a.title')?.getAttribute('href') : '';
+        const noteLink = titleElement ? 'https://www.xiaohongshu.com' + ele.querySelector('a')?.getAttribute('href'):'';
         if(this._extractedLinks.has(noteLink)){
             return;
         }
         // 提取作者和作者链接
-        const authorElement = ele.querySelector('div.author-wrapper a.author') as HTMLAnchorElement;
+        const authorElement = ele.querySelector('.footer div.author-wrapper a.author') as HTMLAnchorElement;
         const authorSpanEle = authorElement.querySelector('span.name') as HTMLSpanElement;
         let author = authorElement ? authorSpanEle.innerText : '';
          // 把#号替换掉，防止csv导出错误
@@ -161,8 +164,24 @@ export default class DataExporter extends Base{
             const likeCount = parseFloat(likeCountStr.replace('w','')) * 10000;
             likeCountStr = likeCount +'';
         }
+        if(likeCountStr === '赞'){
+            likeCountStr = '0';
+        }
+        // 提取是否违规
+        let illegal = '否';
+        const tagArea = ele.querySelector('.bottom-tag-area');
+        if(tagArea && tagArea.innerHTML.includes('违规')){
+            illegal = '是'
+        }
         // 将提取的数据添加到数组中
-        this._data.push([title?title:'无', noteLink?noteLink:'无', author?author:'无', authorLink?authorLink:'无', likeCountStr]);
+        this._data.push([
+            title?title:'无',
+            noteLink?noteLink:'无',
+            author?author:'无',
+            authorLink?authorLink:'无',
+            likeCountStr,
+            illegal
+        ]);
         // 将笔记链接添加到已提取的链接集合中
         this._extractedLinks.add(noteLink);
         // 标记为已提取
